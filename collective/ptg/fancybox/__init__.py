38761daca37227924e605b49cf0ebd757de6b967
Espen Moe-Nilssen <espen@medialog.no>
2016-05-12 23:30:19 +0200

    adding beta settings

--- collective/ptg/fancybox/__init__.py
+++ collective/ptg/fancybox/__init__.py
@@ -1,9 +1,13 @@
+from zope import schema
 from zope.i18nmessageid import MessageFactory
-from collective.plonetruegallery.utils import createSettingsFactory
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from collective.plonetruegallery.browser.views.display import \
     BatchingDisplayType
+from collective.plonetruegallery.browser.views.display import BaseDisplayType
 from collective.plonetruegallery.browser.views.display import jsbool
 from collective.plonetruegallery.interfaces import IBaseSettings
+from collective.plonetruegallery.utils import createSettingsFactory
+
 
 _ = MessageFactory('collective.ptg.fancybox')
 
@@ -13,20 +17,18 @@ class IFancyBoxDisplaySettings(IBaseSettings):
             default=u"Use version 3 beta instead of version 2 stable"),
         default=False)
     caption = schema.Choice(
-        title = _(u"label_caption"),
+        title = _(u"label_caption",
             default=u"Where to show caption"),
-            vocabulary=SimpleVocabulary([
+        vocabulary=SimpleVocabulary([
             SimpleTerm("float", "float",
                 _(u"float", default=u"float")),
             SimpleTerm("outside", "outside",
                 _(u"outside", default=u"outside")),
             SimpleTerm("inside", "inside",
                 _(u"inside", default=u"inside")),
-            SimpleTerm("outside", "outside",
-                _(u"outside", default=u"outside")),
             SimpleTerm("over", "over",
                 _(u"over", default=u"over"))
-        ])
+        ]))
 
 class FancyBoxDisplayType(BatchingDisplayType):
 
@@ -35,9 +37,11 @@ class FancyBoxDisplayType(BatchingDisplayType):
     description = _(u"label_fancybox_display_type",
         default=u"Fancy Box")
     typeStaticFilesRelative = '++resource++ptg.fancybox'
-    fancybox_url = %(base_url) + '/jquery.fancybox'
-    if self.beta:
-        fancybox_url = %(staticFiles) + '/jquery.fancybox3'
+
+    def fancybox_url(self):
+        if self.settings.beta:
+            return '++resource++ptg.fancybox/jquery.fancybox3'
+        return  '++resource++ptg.fancybox/jquery.fancybox'
 
     def javascript(self):
         return u"""
@@ -45,7 +49,7 @@ class FancyBoxDisplayType(BatchingDisplayType):
     src="%(base_url)s/jquery.easing.js"></script>
 <script type="text/javascript"
     src="%(base_url)s/jquery.mousewheel.js"></script>
- <script type="text/javascript" src="%(facebook_url)s.js"></script>
+ <script type="text/javascript" src="%(fancybox_url)s.js"></script>
   <script type="text/javascript">
     var auto_start = %(start_automatically)s;
     var start_image_index = %(start_index_index)i;
@@ -70,20 +74,24 @@ class FancyBoxDisplayType(BatchingDisplayType):
             'start_automatically': jsbool(
                 self.settings.start_automatically or self.settings.timed),
             'start_index_index': self.start_image_index,
-            'staticFiles': self.typeStaticFiles,
-            'caption' : self.caption,
-            'beta' : self.beta,
+            'staticFiles':  self.staticFiles,
+            'caption' : self.settings.caption,
+            'beta' : self.settings.beta,
+            'fancybox_url' : self.fancybox_url(),
+            'base_url': self.typeStaticFiles,
         }
 
     def css(self):
         return u"""
 <link rel="stylesheet" type="text/css"
-    href="%(facebook_url)s.css"> media="screen" />
+    href="%(fancybox_url)s.css"> media="screen" />
     <style>
     #content  a.fancyzoom-gallery {
         border-bottom: 0 none;
     }
     </style>
-
-""" % {'staticFiles': self.typeStaticFiles}
+""" % {
+        'staticFiles': self.typeStaticFiles,
+        'fancybox_url' : self.fancybox_url(),
+        }
 FancyBoxSettings = createSettingsFactory(FancyBoxDisplayType.schema)
